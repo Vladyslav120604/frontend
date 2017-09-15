3e08f15243727604b55b47a7ba49a993838403ed
Vladyslav120604 <serheyev_vladyslav@gymnasium9.kr.ua>
2017-09-15 20:28:39 +0300

    mySQL chat ready

--- mySQL/chatPage/setMsg.php
+++ mySQL/chatPage/setMsg.php
@@ -1,10 +1,11 @@
 <?php  
  	$name = $_POST['name'];
  	$msg = $_POST['msg'];
-
+ 	$time = $_POST['jsTime'];
+ 	echo $time;
  	$user = 'root';
     $pass = '';
     $dbh = new mysqli('127.0.0.1', $user, $pass, 'users');
    // $dbh->query("SELECT * FROM `messages`");
- 	$dbh-> query ("INSERT INTO `messages` (`time`, `name`, `message`) VALUES ('".time()."', '".$name."', '".$msg."')");	
+ 	$dbh-> query ("INSERT INTO `messages` (`jsTime`,`time`, `name`, `message`) VALUES ('".$time."' ,'".time()."', '".$name."', '".$msg."')");	
 ?>
