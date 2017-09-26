1de9dbd81c43af3b8f59ce10bf1af591e6ec7804
Vladyslav120604 <serheyev_vladyslav@gymnasium9.kr.ua>
2017-09-26 15:32:52 +0300

    task_8 ready

--- chat/chatPage/getHistory.php
+++
@@ -1,4 +0,0 @@
-<?php  
-	$msg = json_decode(file_get_contents("history.json"), true);
-	echo json_encode($msg);
-?>
--- chat/msg.php
+++
@@ -1,9 +0,0 @@
-<?php  
- 	$msg = $_POST['msg'];
- 	$email = $_POST['email'];
- 	$a = json_decode(file_get_contents("history.json"), true);
- 	$a = array();
- 	$a[$email] = $msg;
- 	file_put_contents("history.json", json_encode($a));
- 	echo $a;
-?>
